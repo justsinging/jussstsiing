fc19537c7c1420cdeb0c2d27ebd1dc13e7549340
justsinging <jbarnes16103@gmail.com>
2025-06-17 18:08:31 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,19 +3,59 @@
 const productos = [
   {
     id: 1,
-    nombre: "Tote Bag Estrella",
-    precio: 5500,
-    imagen: "img/tote1.jpg",
-    imagenes: ["img/tote1.jpg", "img/tote1b.jpg"],
-    descripcion: "Tote bag 100% algodón con diseño de estrella. Medidas 35x40cm."
+    nombre: "Bolsa Mujer",
+    precio: 18000,
+    imagen: "https://i.imgur.com/bMZdpKo.jpeg",
+    imagenes: ["https://i.imgur.com/QjtmBJJ.jpeg"],
+    descripcion: "Medidas: 25x29 - Telas: Tusor Mostaza - Cinta de algodón natural"
   },
   {
     id: 2,
-    nombre: "Tote Bag Luna",
-    precio: 5700,
-    imagen: "img/tote2.jpg",
+    nombre: "Bolsa Mida",
+    precio: 19000,
+    imagen: "https://i.imgur.com/RTeouUR.jpeg",
     imagenes: ["img/tote2.jpg", "img/tote2b.jpg"],
-    descripcion: "Bolso ecológico con estampado lunar. Muy resistente."
+    descripcion: "Medidas: 46x33 - Telas: Tusor Gris - Cinta de algodón natural"
+  }
+   {
+    id: 3,
+    nombre: "Bolsa 13",
+    precio: 13000,
+    imagen: "https://i.imgur.com/BTUjfH7.jpeg ",
+    imagenes: ["https://i.imgur.com/5qZhCNO.jpeg "],
+    descripcion: "Medidas: 28x33 - Telas: Tusor Gris - Gris oscuro - Cinta de algodón natural"
+  }
+ {
+    id: 4,
+    nombre: "Bolsa Lavanda",
+    precio: 16500,
+    imagen: "https://i.imgur.com/B6E9D7j.jpeg",
+    imagenes: ["https://i.imgur.com/XdEL6D9.jpeg"],
+    descripcion: "Medidas: 33x34 - Telas: Tusor Gris - Cinta de algodón natural"
+  }
+ {
+    id: 5,
+    nombre: "Bolsa Florero",
+    precio: 20000,
+    imagen: "https://i.imgur.com/MTxPBeC.jpeg",
+    imagenes: ["https://i.imgur.com/l2vmhs6.jpeg"],
+    descripcion: "Medidas: 50x46 - Telas: Tusor Mostaza - Cinta de algodón natural"
+  }
+ {
+    id: 6,
+    nombre: "Bolsa Olivo",
+    precio: 13000,
+    imagen: "https://i.imgur.com/z1EjOsG.jpeg",
+    imagenes: ["https://i.imgur.com/xazPdir.jpeg", "https://i.imgur.com/xazPdir.jpeg"],
+    descripcion: "Medidas: 34x28 - Telas: Tusor Verde - Cinta de algodón natural"
+  }
+ {
+    id: 7,
+    nombre: "Bolsa Frutis",
+    precio: 16500,
+    imagen: "https://i.imgur.com/YXYUHHr.jpeg",
+    imagenes: ["https://i.imgur.com/DzJc6iO.jpeg"],
+    descripcion: "Medidas: 28x33 - Telas: Tusor Mostaza - Cinta de algodón natural"
   }
   // Agregá más productos con las mismas claves si querés
 ];
