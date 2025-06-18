f082d07b152c17a3e8cf8452b012088d04a3f03d
justsinging <jbarnes16103@gmail.com>
2025-06-18 11:05:51 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // main.js
-
-const productos = [
+ const productos = [
   {
     id: 1,
     nombre: "Bolsa Mujer",
@@ -16,40 +15,40 @@ const productos = [
     imagen: "https://i.imgur.com/RTeouUR.jpeg",
     imagenes: ["img/tote2.jpg", "img/tote2b.jpg"],
     descripcion: "Medidas: 46x33 - Telas: Tusor Gris - Cinta de algodón natural"
-  }
-   {
+  },
+  {
     id: 3,
     nombre: "Bolsa 13",
     precio: 13000,
-    imagen: "https://i.imgur.com/BTUjfH7.jpeg ",
-    imagenes: ["https://i.imgur.com/5qZhCNO.jpeg "],
+    imagen: "https://i.imgur.com/BTUjfH7.jpeg",
+    imagenes: ["https://i.imgur.com/5qZhCNO.jpeg"],
     descripcion: "Medidas: 28x33 - Telas: Tusor Gris - Gris oscuro - Cinta de algodón natural"
-  }
- {
+  },
+  {
     id: 4,
     nombre: "Bolsa Lavanda",
     precio: 16500,
     imagen: "https://i.imgur.com/B6E9D7j.jpeg",
     imagenes: ["https://i.imgur.com/XdEL6D9.jpeg"],
     descripcion: "Medidas: 33x34 - Telas: Tusor Gris - Cinta de algodón natural"
-  }
- {
+  },
+  {
     id: 5,
     nombre: "Bolsa Florero",
     precio: 20000,
     imagen: "https://i.imgur.com/MTxPBeC.jpeg",
     imagenes: ["https://i.imgur.com/l2vmhs6.jpeg"],
     descripcion: "Medidas: 50x46 - Telas: Tusor Mostaza - Cinta de algodón natural"
-  }
- {
+  },
+  {
     id: 6,
     nombre: "Bolsa Olivo",
     precio: 13000,
     imagen: "https://i.imgur.com/z1EjOsG.jpeg",
-    imagenes: ["https://i.imgur.com/xazPdir.jpeg", "https://i.imgur.com/xazPdir.jpeg"],
+    imagenes: ["https://i.imgur.com/xazPdir.jpeg"],
     descripcion: "Medidas: 34x28 - Telas: Tusor Verde - Cinta de algodón natural"
-  }
- {
+  },
+  {
     id: 7,
     nombre: "Bolsa Frutis",
     precio: 16500,
@@ -57,6 +56,7 @@ const productos = [
     imagenes: ["https://i.imgur.com/DzJc6iO.jpeg"],
     descripcion: "Medidas: 28x33 - Telas: Tusor Mostaza - Cinta de algodón natural"
   }
+];
   // Agregá más productos con las mismas claves si querés
 ];
 
